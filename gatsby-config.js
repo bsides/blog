b54cc38c95178290f0e2fcabe311c4f311bf946d
Rafael Pereira <rafaelbsides@gmail.com>
2018-12-07 21:12:49 -0200

    Removendo plugin até resolver o que está acontecendo

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,7 +92,8 @@ module.exports = {
         icon: `src/assets/profile.jpg`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
     `gatsby-plugin-react-helmet`,
   ],
 }
